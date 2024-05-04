12edb2a49a8f4ef5a19562043811c7a497c41287
claudiosanto <santosclaudio23acevedo@gmail.com>
2024-05-04 14:29:46 -0300

    first commit

---
+++ src/context/Context.jsx
@@ -0,0 +1,45 @@
+import { createContext, useContext, useState } from "react";
+//import { db } from "../../firebase/config";
+//import { collection, getDocs } from "firebase/firestore";
+// creo un context
+export const ProductsContext = createContext();
+
+//funcionalidad para usar contexto mas facil
+
+export const useProductContext = () => useContext(ProductsContext);
+
+//provider provee al resto de nuestra aplicacion
+function ProductsProvider({ children }) {
+  //proveedor de productos " ProductsProvider"
+  //creas un estado global
+  const [products, setProducts] = useState([]);
+
+  const GetProducts = async () => {
+    try {
+      // traer datos con fetch
+      const response = await fetch("https://fakestoreapi.com/products/");
+      const data = await response.json();
+      console.log(data);
+      setProducts(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const GetElementById = async (Id) => {
+    try {
+      const response = await fetch(`https://fakestoreapi.com/products/${Id}`);
+      const data = await response.json();
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <ProductsContext.Provider value={{ products, GetProducts, GetElementById }}>
+      {children}
+    </ProductsContext.Provider>
+  );
+}
+
+export default ProductsProvider;
---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+
+/*import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCbvLzeYTv_wGOadmnrykwJGhK7AY5Nd4U",
+  authDomain: "aula-14-b83e6.firebaseapp.com",
+  projectId: "aula-14-b83e6",
+  storageBucket: "aula-14-b83e6.appspot.com",
+  messagingSenderId: "995876567312",
+  appId: "1:995876567312:web:e56e0ca8eeb32d22538d95"
+};
+
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);*/
